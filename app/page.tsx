843f3618f58e4d1b68ea50097be5546695b8f39e
Oleksandr Kashytskyi <sashakashytskyy@gmail.com>
2026-02-26 21:27:54 +0000

    init

---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+function Profile() {
+  return (
+      <img
+          src="https://i.imgur.com/MK3eW3As.jpg"
+          alt="Katherine Johnson"
+      />
+  );
+}
+
+export default function Home() {
+  return (
+      <main>
+        <>
+          <h1>Hedy Lamarr's Todos</h1>
+          <img
+              src="https://i.imgur.com/yXOvdOSs.jpg"
+              alt="Hedy Lamarr"
+              className="w-40 h-40 rounded-full object-cover"
+          />
+          <ul className="list-disc pl-6">
+            <li>Invent new traffic lights</li>
+            <li>Rehearse a movie scene</li>
+            <li>Improve spectrum technology</li>
+          </ul>
+        </>
+      </main>
+  );
+}
