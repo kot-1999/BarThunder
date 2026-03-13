39cd5a1168b9bc968077c0409999c1c50bd9d911
Oleksandr Kashytskyi <sashakashytskyy@gmail.com>
2026-03-13 10:26:02 +0000

    footer was added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,11 @@
 
 import "./globals.css";
 
-import { Layout, Menu } from "antd";
+import {Layout, Menu, Space} from "antd";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import {Footer} from "antd/es/layout/layout";
+import Text from "antd/es/typography/Text";
 
 const { Header, Content } = Layout;
 
@@ -26,7 +28,7 @@ export default function RootLayout({children}: {
                             { key: "/", label: <Link href="/">Home</Link> },
                             { key: "/content", label: <Link href="/content">Content</Link> },
                             { key: "/about", label: <Link href="/about">About</Link> },
-                            { key: "/drinks/new", label: <Link href="/app/cocktail/new">Add Cocktail</Link> },
+                            { key: "/drinks/new", label: <Link href="/cocktail/new">Add Cocktail</Link> },
                             { key: "/login", label: <Link href="/login">Login</Link> },
                         ]}
                     />
@@ -35,6 +37,28 @@ export default function RootLayout({children}: {
                 <Content style={{ padding: 24 }}>
                     {children}
                 </Content>
+
+                <Footer style={{ textAlign: "center", marginTop: 40 }}>
+                    <Space direction="vertical" size={4}>
+                        <Text>
+                            🍸 Cocktail Library — Discover and explore cocktail recipes
+                        </Text>
+
+                        <Space size="large">
+                            <Link href="/">Home</Link>
+                            <Link href="https://demo.barassistant.app/bar/docs#/" target="_blank">
+                                Bar Assistant API
+                            </Link>
+                            <Link href="https://github.com/kot-1999/BarThunder" target="_blank">
+                                GitHub
+                            </Link>
+                        </Space>
+
+                        <Text type="secondary">
+                            © {new Date().getFullYear()} Bar Thunder App
+                        </Text>
+                    </Space>
+                </Footer>
             </Layout>
         </body>
         </html>
