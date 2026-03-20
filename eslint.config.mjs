822326b211571dc048594869c4b275d6f95bf9eb
Oleksandr Kashytskyi <sashakashytskyy@gmail.com>
2026-03-20 18:01:57 +0000

    eslint config was updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,11 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "/no-explicit-any": "warn"
+    }
+  }
 ]);
 
 export default eslintConfig;
