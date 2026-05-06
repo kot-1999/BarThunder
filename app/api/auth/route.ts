ae7954bc1108bce1d239a5ae725ec98c02c13d79
Oleksandr Kashytskyi <sashakashytskyy@gmail.com>
2026-05-06 15:12:17 +0100

    Logout was added

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -2,6 +2,7 @@
 
 import { api } from "@/app/src/ApiRequests";
 import {getCookie, handleServerError, IError} from "@/app/src/server";
+import {NextResponse} from "next/server";
 
 export async function POST(req: Request) {
     try {
@@ -22,11 +23,24 @@ export async function POST(req: Request) {
     }
 }
 
+export async function PUT() {
+    try {
+        const res = api.logout()
+
+        const response = NextResponse.json({}, { status: 200 });
+        response.cookies.delete("userToken");
+        response.cookies.delete("userData");
+        return response;
+    } catch (err: IError | any) {
+        return handleServerError(err)
+    }
+}
+
 export async function GET() {
     try {
         const isAuthenticated = await getCookie('userToken')
 
-        return new Response(JSON.stringify({ isAuthenticated }), { status: 200 })
+        return new Response(JSON.stringify({ isAuthenticated: !!isAuthenticated }), { status: 200 })
 
     } catch (err: IError | any) {
         return handleServerError(err)
